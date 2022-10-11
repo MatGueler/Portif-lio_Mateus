a74899ad19b493cca408d2f0b29d524c2f42b706
MattGueler <mateus.gueler@gmail.com>
2022-10-11 13:11:11 -0300

    feat:
    Initialize repository

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "../../Assets/CSS/GlobalStyle";
+import InitialScreen from "../Pages/InitialScreen/InitialScreen";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes>
+          <Route path={"/MateusGueler"} element={<InitialScreen />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Pages/InitialScreen/InitialScreen.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+function InitialScreen() {
+  return <></>;
+}
+
+export default InitialScreen;
